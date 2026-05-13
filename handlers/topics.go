c3f488353caf5ffb07e2ca869d6846897e923ca4
appleofeden110 <210235386@aston.ac.uk>
2026-05-13 18:16:19 +0100

    print

--- handlers/topics.go
+++ handlers/topics.go
@@ -233,6 +233,9 @@ func GetAllSuperAdminsOfGroup(groupChatId int64, users []*db.User) ([]*db.User, 
 		fmt.Println("User ids while finding super admin: ", us.Id, us.ChatId, us.Name)
 		resp, err := Bot.Request(tgbotapi.GetChatMemberConfig{ChatConfigWithUser: tgbotapi.ChatConfigWithUser{ChatID: groupChatId, UserID: us.ChatId}})
 		if err != nil {
+			if strings.Contains(err.Error(), "PARTICIPANT_ID_INVALID") {
+				continue
+			}
 			if strings.Contains(err.Error(), "user not found") {
 				continue
 			}
